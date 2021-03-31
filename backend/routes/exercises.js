e2b4b6a54a200e16557645b487592bbf10fbc03f
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2021-03-31 16:09:14 +0530

    finding id's by findbyid

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -25,13 +25,13 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:id!').get((req, res) => {
+router.route('/:id').get((req, res) => {
     Exercise.findById(req.params.id)
-        .then(() => res.json(exercise))
+        .then(exercise => res.json(exercise))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:id!').delete((req, res) => {
+router.route('/:id').delete((req, res) => {
     Exercise.findByIdAndDelete(req.params.id)
         .then(() => res.json('Exercise deleted.'))
         .catch(err => res.status(400).json('Error: ' + err));
