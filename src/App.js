bab6ade78058af0cb9d73dfd5110fe6aa7afbdff
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2021-04-01 14:52:11 +0530

    rendering all components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Navbar from "./components/navbarcomponent";
+import ExerciseList from "./components/exercises-list-component";
+import EditExercise from "./components/edit-exercises-component";
+import CreateExercise from "./components/create-exercises-component";
+import CreateUser from "./components/create-user-component";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExerciseList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
+    
   )
 }
 
